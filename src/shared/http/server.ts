65b069e2d70cabeb598e533f754991935acf7768
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-12-09 08:57:22 -0300

    feat: create initial configure to run server

---
+++ src/shared/http/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import router from './routes';
+
+const app = express();
+
+try {
+  const PORT = 3001;
+  app.use(cors());
+  app.use(express.json());
+  app.use(router);
+  app.listen(PORT, () => {
+    console.log(`🔥 Server listening in http://localhost:${PORT}`);
+  });
+} catch (e) {
+  console.log(e);
+}
