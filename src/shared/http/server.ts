611cee7e7984db2a0ae375870407788eca8e6727
Joao Pedro Prudêncio <joaopprudencio882@gmail.com>
2022-12-12 00:18:41 -0300

    feat: using celebrate errors in server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import router from './routes';
+import { errors } from 'celebrate';
 import errorHandler from './middlewares/errorHandler';
+
+import '@shared/database';
 
 const app = express();
 
@@ -10,6 +15,7 @@ try {
   app.use(cors());
   app.use(express.json());
   app.use(router);
+  app.use(errors());
   app.use(errorHandler);
   app.listen(PORT, () => {
     console.log(`🔥 Server listening in http://localhost:${PORT}`);
