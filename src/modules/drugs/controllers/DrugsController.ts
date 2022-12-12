4be6773841c4f8e99f535f91e13254c65166ada7
Joao Pedro Prudêncio <joaopprudencio882@gmail.com>
2022-12-12 00:11:12 -0300

    feat: create drugs controller

---
+++ src/modules/drugs/controllers/DrugsController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+
+import CreateDrugService from '../services/CreateDrugService';
+import DeleteDrugService from '../services/DeleteDrugService';
+import ListDrugService from '../services/ListDrugService';
+import ShowDrugService from '../services/ShowDrugService';
+import UpdateDrugService from '../services/UpdateDrugService';
+
+export default class DrugsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listDrugService = new ListDrugService();
+
+    const drugs = listDrugService.execute();
+
+    return response.status(200).json(drugs);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showDrugService = new ShowDrugService();
+
+    const drug = await showDrugService.execute({ id });
+
+    return response.status(200).json(drug);
+  }
+
+  public async store(request: Request, response: Response): Promise<Response> {
+    const { name, shortDescription } = request.body;
+
+    const createDrugService = new CreateDrugService();
+
+    const drug = await createDrugService.execute({ name, shortDescription });
+
+    return response.status(200).json(drug);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, shortDescription } = request.body;
+
+    const updateDrugService = new UpdateDrugService();
+
+    await updateDrugService.execute({
+      id,
+      data: { name, shortDescription },
+    });
+
+    return response.status(201);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteDrugService = new DeleteDrugService();
+
+    await deleteDrugService.execute({ id });
+
+    return response.status(201);
+  }
+}
