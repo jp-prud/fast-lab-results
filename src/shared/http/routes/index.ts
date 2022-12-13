4a6318173697ec12498c5b449bf2dfe50600491b
Joao Pedro Prudêncio <jpprud.contato@gmail.com>
2022-12-12 23:31:41 -0300

    feat: use recipes routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
-import drugsRouter from '@modules/drugs/routes/drugs.routes';
 import { Router } from 'express';
+
+import drugsRouter from '@modules/drugs/routes/drugs.routes';
+import recipesRouter from '@modules/recipes/routes/recipes.routes';
 
 const router = Router();
 
 router.use('/drugs', drugsRouter);
+router.use('/recipes', recipesRouter);
 
 export default router;
