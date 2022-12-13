23c40deda643ba885f733241e0285b5d01c0b3bb
Joao Pedro Prudêncio <jpprud.contato@gmail.com>
2022-12-12 23:27:51 -0300

    feat: create consumer entity

---
+++ src/modules/consumers/entities/Consumer.ts
@@ -0,0 +1,36 @@
+import Recipe from '@modules/recipes/typeorm/entities/Recipe';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('consumer')
+export default class Consumer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true, length: 150 })
+  name: string;
+
+  @Column({ unique: true, length: 150 })
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  age: number;
+
+  @OneToMany(type => Recipe, consumer => Consumer, { eager: true })
+  recipes: Recipe[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
