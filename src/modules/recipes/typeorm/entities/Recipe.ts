f00b6fadcb1ebe0e3e77fc1ee18e63b81f8c09e2
Joao Pedro Prudêncio <jpprud.contato@gmail.com>
2022-12-12 23:31:19 -0300

    feat: recipe entity

---
+++ src/modules/recipes/typeorm/entities/Recipe.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+import Consumer from '@modules/consumers/entities/Consumer';
+import Drug from '@modules/drugs/typeorm/entities/Drug';
+
+@Entity('recipe')
+export default class Recipe {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 150, unique: true })
+  title: string;
+
+  @Column()
+  shortDescription: string;
+
+  @Column({ length: 100 })
+  medicName: string;
+
+  @Column()
+  documentPath: string;
+
+  @ManyToOne(type => Drug, recipes => Recipe, { eager: true })
+  relatedDrugs: Drug;
+
+  @ManyToOne(type => Consumer, recipes => Recipe)
+  consumer: Consumer;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+}
