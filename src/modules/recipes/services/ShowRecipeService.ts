4264173ee4cdff927059e7efb0402e1388686836
Joao Pedro Prudêncio <jpprud.contato@gmail.com>
2022-12-13 23:01:12 -0300

    refactor: change method in service to show recipe

--- src/modules/recipes/services/ShowRecipeService.ts
+++ src/modules/recipes/services/ShowRecipeService.ts
@@ -1,17 +1,19 @@
+import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import { RecipeRepository } from '../repositories/RecipesRepository';
+import Recipe from '../typeorm/entities/Recipe';
 
 interface IRequest {
   id: string;
 }
 
 export default class ShowRecipeService {
-  public async execute({ id }: IRequest) {
+  public async execute({ id }: IRequest): Promise<Recipe> {
     const recipeRepository = getCustomRepository(RecipeRepository);
 
-    const recipe = await recipeRepository.find({ id });
+    const recipe = await recipeRepository.findOne(id);
 
-    if (!recipe) throw new Error('Recipe not found');
+    if (!recipe) throw new AppError('Recipe not found');
 
     return recipe;
   }
