0d8f6e400d1f409d44ae4ce2087aa39198c47d17
Joao Pedro Prudêncio <jpprud.contato@gmail.com>
2022-12-23 21:48:01 -0300

    style(fe): create home page

---
+++ fe/src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import Input from '../../components/input';
+
+export const InputSearchContainer = styled.div`
+  width: 100%;
+`;
+
+export const SearchInput = styled(Input)`
+  height: 50px;
+  border-radius: 25px;
+`;
