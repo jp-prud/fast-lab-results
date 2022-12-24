c6978b9583886fc30ee9ce91b1cd86f030da5199
Joao Pedro Prudêncio <jpprud.contato@gmail.com>
2022-12-23 21:54:15 -0300

    style(fe): create global header

---
+++ fe/src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  margin-block: 74px 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
